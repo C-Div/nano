2cc944cd17242fd768e170cac6e731bb7f475d49
= <=>
2026-06-30 05:19:36 -0500

    Added Nano First Person Model config

---
+++ src/client/java/cdiv/nano/client/api/config/FirstPersonModel.java
@@ -0,0 +1,13 @@
+package cdiv.nano.client.api.config;
+
+import cdiv.nano.api.Option;
+
+/**
+ * <p>Configuration related to the First Person Model mod</p>
+ */
+public class FirstPersonModel {
+    /**
+     * <p>Whether the body offset scales with {@link virtuoel.pehkui.api.ScaleData}.</p>
+     */
+    public static Option<Boolean> bodyOffsetScalingEnabled = new Option<>(true);
+}
